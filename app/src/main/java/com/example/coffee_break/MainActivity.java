0e595c6c394b2529fca3c95929414cce12ea530a
ntotmyanin <ntotmyanin@cinimex.ru>
2023-02-17 22:36:27 +0300

    [add]: toast in lyfecycle methods

--- app/src/main/java/com/example/coffee_break/MainActivity.java
+++ app/src/main/java/com/example/coffee_break/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.coffee_break;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected Toast getToast(CharSequence text, int duration) {
+        Context context = getApplicationContext();
+
+        return Toast.makeText(context, text, duration);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,22 +23,27 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
+        getToast("onStart", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onStop() {
         super.onStop();
+        getToast("onStop", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        getToast("onDestroy", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onPause() {
         super.onPause();
+        getToast("onPause", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onResume() {
         super.onResume();
+        getToast("onResume", Toast.LENGTH_SHORT).show();
     }
 
 }
